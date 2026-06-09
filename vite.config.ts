331a5b15cccfcdcf253a1b09b2fe0165e99cd823
Manvendra Singh <manvendrasingh231105@gmail.com>
2026-06-09 15:11:31 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,10 @@ import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
+  const isProduction = mode === 'production';
+
   return {
-    base: './',
+    base: isProduction ? '/Portfolio-web/' : '/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
